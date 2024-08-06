65af58f4519f72609579eb6b0691971cea26fe6f
Koschei <nitianzero@gmail.com>
2024-08-06 21:28:02 +0800

    完成21

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -11,9 +11,14 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (unsigned int i = 0; i < N; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
+
     ~Tensor() {
         delete[] data;
     }
@@ -25,6 +30,7 @@ struct Tensor {
     T &operator[](unsigned int const indices[N]) {
         return data[data_index(indices)];
     }
+
     T const &operator[](unsigned int const indices[N]) const {
         return data[data_index(indices)];
     }
@@ -32,9 +38,15 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
+        unsigned int stride = 1;
         for (unsigned int i = 0; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
+            stride = 1;
+            for (unsigned int j = i + 1; j < N; ++j) {
+                stride *= shape[j];
+            }
+            index += stride * indices[i];
         }
         return index;
     }
